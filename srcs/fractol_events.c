2bb0e6d115e15583b05dd6d4bde984c4cca9a9f9
Lavi Iovino <lalliovino@gmail.com>
2025-02-17 13:39:37 +0100

    salva mouse

--- srcs/fractol_events.c
+++ srcs/fractol_events.c
@@ -6,7 +6,7 @@
 /*   By: liovino <liovino@student.42.it>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 18:42:52 by liovino           #+#    #+#             */
-/*   Updated: 2025/02/06 20:04:01 by liovino          ###   ########.fr       */
+/*   Updated: 2025/02/17 12:01:13 by liovino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,42 @@ int	manage_exit(t_fractal *fractal)
 	exit(EXIT_SUCCESS);
 }
 
-int	manage_key(int key, t_fractal *fractal)
+int	manage_some(int key, t_fractal *fractal)
 {
 	if (key == XK_Escape)
 		manage_exit(fractal);
 	if (key == XK_Left)
-		fractal->shift_x -= (0.5 * fractal->zoom);
+		fractal->shift_x -= (fractal->max.rl_x - fractal->min.rl_x) \
+							* 0.05;
 	else if (key == XK_Up)
-		fractal->shift_y += (0.5 * fractal->zoom);
+		fractal->shift_y -= (fractal->max.im_y - fractal->min.im_y) \
+							* 0.05;
 	else if (key == XK_Right)
-		fractal->shift_x += (0.5 * fractal->zoom);
+		fractal->shift_x += (fractal->max.rl_x - fractal->min.rl_x) \
+							* 0.05;
 	else if (key == XK_Down)
-		fractal->shift_y -= (0.5 * fractal->zoom);
+		fractal->shift_y += (fractal->max.im_y - fractal->min.im_y) \
+							* 0.05;
 	else if (key == XK_plus)
 		fractal->image_def += 10;
 	else if (key == XK_minus)
 		fractal->image_def -= 10;
+	fractal_render(fractal);
+	return (0);
+}
+
+int	manage_more(int key, t_fractal *fractal)
+{
+	bool	redraw;
+
+	redraw = 0;
+	if (key == 65288)
+		fractal->motion = !fractal->motion;
+	else if (key == 32)
+	{
+		fractal->smooth = !fractal->smooth;
+		redraw = 1;
+	}
 	else if (key == XK_Tab)
 	{
 		if (fractal->palette.val < 2)
@@ -44,28 +64,46 @@ int	manage_key(int key, t_fractal *fractal)
 		else
 			fractal->palette.val = 1;
 		palette_def(&fractal->palette, fractal);
+		redraw = 1;
 	}
-	fractal_render(fractal);
+	if (redraw)
+		fractal_render(fractal);
 	return (0);
 }
 
 int	manage_mouse(int button, int x, int y, t_fractal *fractal)
 {
-	(void)x;
-	(void)y;
+	double		zm;
+	t_complex	mouse;
+//	t_complex	d;
 
+	zm = 0;
+	mouse.rl_x = scaling(x, WIDTH, fractal->min.rl_x, fractal->max.rl_x);
+	mouse.im_y = scaling(y, HEIGHT, fractal->min.im_y, fractal->max.im_y);
 	if (button == Button5)
-		fractal->zoom *= 0.95;
+		zm = 1.05;
 	else if (button == Button4)
-		fractal->zoom *= 1.05;
+		zm = 0.95;
+	else
+		return (0);
+	fractal->min.rl_x = mouse.rl_x + (fractal->min.rl_x - mouse.rl_x) * zm;
+	fractal->max.rl_x = mouse.rl_x + (fractal->max.rl_x - mouse.rl_x) * zm;
+	fractal->min.im_y = mouse.im_y + (fractal->min.im_y - mouse.im_y) * zm;
+	fractal->max.im_y = mouse.im_y + (fractal->max.im_y - mouse.im_y) * zm;
+	fractal->zoom *= zm;
 	fractal_render(fractal);
-	return(0);
+	return (0);
 }
 
 int	manage_track(int x, int y, t_fractal *fractal)
 {
-	fractal->julia_x = (scaling(x,  0, WIDTH, -2.0, 2.0) * fractal->zoom) + fractal->shift_x;
-	fractal->julia_y = (scaling(y,  0, HEIGHT, 2.0, -2.0) * fractal->zoom) + fractal->shift_y;
-	fractal_render(fractal);
+	if (fractal->motion)
+	{
+		fractal->julia_x = (scaling(x, WIDTH, fractal->min.rl_x, \
+				fractal->max.rl_x) * fractal->zoom) + fractal->shift_x;
+		fractal->julia_y = (scaling(y, HEIGHT, fractal->min.im_y, \
+				fractal->max.im_y) * fractal->zoom) + fractal->shift_y;
+		fractal_render(fractal);
+	}
 	return (0);
 }
